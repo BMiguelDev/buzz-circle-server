0b98b0f73fddb5b2b5f4d6176b7a6e939ce04347
Bruno Miguel <113399299+BMiguelDev@users.noreply.github.com>
2023-10-12 18:57:30 +0100

    Fix frontend app URL in allowed origins list

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://bmigueldev.github.io/buzz-circle',
+    'https://bmigueldev.github.io',
     // 'http://127.0.0.1:5500',    // Only for development
     // 'http://localhost:3500',    // Only for development
     // 'http://localhost:3000'     // Only for development
